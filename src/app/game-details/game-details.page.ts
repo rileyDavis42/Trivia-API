2906e8446986e5165eb527349b5e6f055c23bea4
Riley Davis <riguyrocks01@gmail.com>
2019-01-24 09:42:26 -0700

    Game details page and service

--- src/app/game-details/game-details.page.ts
+++ src/app/game-details/game-details.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../user/user';
+import { GameService } from '../user/game.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
     selector: 'app-game-details',
@@ -10,12 +12,25 @@ import { User } from '../user/user';
 export class GameDetailsPage implements OnInit {
 
     user: User;
+    categories: Object;
+    data = {
+        categoryID: '',
+        questions: 0,
+        difficulty: 'medium'
+    };
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute, private gameService: GameService, private navCtrl: NavController) {
     }
 
     ngOnInit() {
         this.user = JSON.parse(this.route.snapshot.paramMap.get('user'));
+        this.gameService.getCategories().subscribe(data => {
+            this.categories = data['trivia_categories'];
+        });
+    }
+
+    startGame() {
+        this.navCtrl.navigateForward(['trivia-page', { data: JSON.stringify(this.data) }]);
     }
 
 }
--- src/app/trivia-page/trivia-page.page.ts
+++ src/app/trivia-page/trivia-page.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-trivia-page',
-  templateUrl: './trivia-page.page.html',
-  styleUrls: ['./trivia-page.page.scss'],
+    selector: 'app-trivia-page',
+    templateUrl: './trivia-page.page.html',
+    styleUrls: ['./trivia-page.page.scss'],
 })
 export class TriviaPagePage implements OnInit {
 
-  constructor() { }
+    data = {
+        categoryID: '',
+        questions: 0,
+        difficulty: ''
+    };
+    isLoaded = false;
 
-  ngOnInit() {
-  }
+    constructor( private route: ActivatedRoute, private navCtrl: NavController ) {
+    }
 
+    ngOnInit() {
+        this.data = JSON.parse(this.route.snapshot.paramMap.get('data'));
+        this.isLoaded = true;
+    }
+
+    goBack() {
+        if (confirm('Exit the quiz?')) {
+            this.navCtrl.goBack();
+        }
+    }
 }
