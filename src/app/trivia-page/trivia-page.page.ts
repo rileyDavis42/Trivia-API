d298cf20d2969e20a9ec4385a4044032fe22b8ae
Valerie <42677050+valeriedludden@users.noreply.github.com>
2019-01-28 10:41:25 -0700

    testing answers

--- src/app/trivia-page/trivia-page.page.ts
+++ src/app/trivia-page/trivia-page.page.ts
@@ -43,6 +43,13 @@ export class TriviaPagePage implements OnInit {
             this.isLoaded = true;
             this.data.gameID = this.userService.startNewGame(this.user, this.data);
             console.log(this.data.questions);
+            this.getAnswers();
+            this.askQuestion();
+            console.log("question" + this.questions[this.count]['question']);
+            // console.log("answers" + this.questions[this.count]['correct_answer'] + this.questions[this.count]['incorrect_answer']);
+            console.log("answers" + this.answers);
+            // console.log("answers[0]" + this.answers[0].question);
+            console.log("correct" + this.questions[this.count]['correct_answer']);
         });
     }
 
@@ -61,15 +68,20 @@ export class TriviaPagePage implements OnInit {
 
     askQuestion() {
         this.activeQuestion = this.questions[this.count]['question'];
-        this.answers = this.getAnswers();
+        // this.answers = this.getAnswers();
     }
     getAnswers() {
         const tempArray = [];
-       tempArray.push({question: this.questions[this.count]['correct_answer'], correct: true});
+       tempArray.push({pAnswer: this.questions[this.count]['correct_answer'], correct: true});
         for (let i = 0; i < this.questions[this.count]['incorrect_answers']; i ++) {
-            tempArray.push({answer: this.questions[this.count]['incorrect_answers'][i], correct: false});
+            tempArray.push({pAnswer: this.questions[this.count]['incorrect_answers'][i], correct: false});
         }
-        return tempArray;
+        console.log("tempArray" + JSON.stringify(tempArray));
+        console.log("tempArray[0]" + JSON.stringify(tempArray[0]));
+        console.log("tempArray answer" + JSON.stringify(tempArray[0].correct));
+        console.log("No JSON answer" + tempArray[0].correct);
+        this.answers = tempArray;
+        console.log("this.answers" + this.answers)
     }
 
     answerQuestion( questionNumb: number, correct: boolean ) {
