6f869c260b497a097e7644502de07a9b43a5d85e
Riley Davis <riguyrocks01@gmail.com>
2019-01-31 10:12:31 -0700

    Started game details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MenuController} from "@ionic/angular";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'game-details', loadChildren: './game-details/game-details.module#GameDetailsPageModule' },
-  { path: 'trivia-page', loadChildren: './trivia-page/trivia-page.module#TriviaPagePageModule' },
-  { path: 'player-stats', loadChildren: './player-stats/player-stats.module#PlayerStatsPageModule' },
-  { path: 'my-games', loadChildren: './my-games/my-games.module#MyGamesPageModule' },
-  { path: '**', loadChildren: './home/home.module#HomePageModule' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+    { path: 'game-details', loadChildren: './game-details/game-details.module#GameDetailsPageModule' },
+    { path: 'game-results', loadChildren: './game-results/game-results.module#GameResultsPageModule' },
+    { path: 'trivia-page', loadChildren: './trivia-page/trivia-page.module#TriviaPagePageModule' },
+    { path: 'player-stats', loadChildren: './player-stats/player-stats.module#PlayerStatsPageModule' },
+    { path: 'my-games', loadChildren: './my-games/my-games.module#MyGamesPageModule' },
+    { path: '**', loadChildren: './home/home.module#HomePageModule' },
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 
--- src/app/game-results/game-results.page.ts
+++ src/app/game-results/game-results.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Game } from '../user/game';
 
 @Component({
-  selector: 'app-game-results',
-  templateUrl: './game-results.page.html',
-  styleUrls: ['./game-results.page.scss'],
+    selector: 'app-game-results',
+    templateUrl: './game-results.page.html',
+    styleUrls: ['./game-results.page.scss'],
 })
 export class GameResultsPage implements OnInit {
 
-  constructor() { }
+    game: Game;
+    questions: Object[];
+    score: number;
 
-  ngOnInit() {
-  }
+    constructor(private route: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        this.game = JSON.parse(this.route.snapshot.paramMap.get('game'));
+        this.questions = this.game.questions;
+        console.log(this.game);
+        this.getScore();
+    }
+
+    getScore() {
+        this.score = 0;
+        for (let i = 0; i < this.questions.length; i++) {
+            if (this.questions[i]['correct']) {
+                this.score ++;
+            }
+        }
+        console.log('Score', this.score);
+    }
 
 }
